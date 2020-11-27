8b991f148b8294db838bfeeca576f4ba2def264f
Addy Joel <sammcaddy@gmail.com>
2020-11-26 23:03:13 -0500

    deploy my react app to github pages

--- src/Option1.js
+++ src/Option1.js
@@ -5,11 +5,9 @@ function Option1() {
     return (
         <div className= "men">
 
-            <img className= "opt_pg_img" src= "pd/oppp.jpg"
+            <img className= "opt_pg_img" src= "/pd/oppp.jpg"
             alt= ""/>
-
-           <img className="home_banner" src="pd/architecture-astronomy-bright-building-267625.jpg"
-            alt= ""/>
+            
             
             <div className="opt_info">
             <h1>Your Heading goes here!</h1>
--- src/Option2.js
+++ src/Option2.js
@@ -7,9 +7,6 @@ function Option2() {
             <img className="opt_pg_img" src="pd/opp.jpg"
             alt= ""/>
             
-            <img className="home_banner" src="pd/architecture-astronomy-bright-building-267625.jpg"
-            alt= ""/>
-            
             <div className="opt_info">
             <h1>Your Heading goes here!</h1>
             <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
